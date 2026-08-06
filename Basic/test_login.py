ce4405b24c55d88a81dc6e6537e42a0d6d031488
melokuhlemazii <melomazibuko8@gmail.com>
2026-08-06 16:59:51 +0200

    Basic login test

--- Basic/test_login.py
+++ Basic/test_login.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class TestLogin:
     web_url = "https://ndosisimplifiedautomation.vercel.app/"
@@ -15,7 +17,11 @@ class TestLogin:
 
         wait = WebDriverWait(self.driver, 10)
 
-
+        wait.until(EC.visibility_of_element_located((By.XPATH, self.home_login_button_xpath))).click()
+        #wait.until(EC.visibility_of_element_located((By.ID, self.username_id_textbox))).send_keys("melomazibuko8@gmail.com")
+        self.driver.find_element(By.ID, self.username_id_textbox).send_keys("melomazibuko8@gmail.com")
+        self.driver.find_element(By.ID, self.password_id_textbox).send_keys("Mwelase@1031")
+        self.driver.find_element(By.ID, self.login_submit_button_id).click()
 
 
 
